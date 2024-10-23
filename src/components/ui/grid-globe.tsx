fc76aa0fdac196cfcb612813b42350c7ea872719
Shraban-7 <shakuatshraban@gmail.com>
2024-10-23 12:51:10 +0600

    "update"

--- src/components/ui/grid-globe.tsx
+++ src/components/ui/grid-globe.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import dynamic from "next/dynamic";
 
-const World = dynamic(() => import("@/components/ui/Globe").then((mod) => mod.World), {
+const World = dynamic(() => import("@/components/ui/globe").then((mod) => mod.World), {
   ssr: false,
   loading: () => <div>Loading...</div>
 });
